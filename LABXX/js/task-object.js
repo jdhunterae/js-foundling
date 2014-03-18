2467957c786e569157365c91978b67d9acc8e7b2
Andrew <arpomerleau@gmail.com>
2014-03-18 19:44:13 -0400

    Update LABXX index and initial Task object file

---
+++ LABXX/js/task-object.js
@@ -0,0 +1,12 @@
+/**********************************************************************
+ ***   Task Object file for HTML/JavaScript Application             ***
+ ***   @author: Andrew Pomerleau                                    ***
+ ***   @date:   02/25/2014                                          ***
+ **********************************************************************/
+
+function Task() {
+  this.name = "";
+  this.description = "";
+  this.due_date = "";
+  this.status = -1;
+}
